4a8e734096ace5588550237a01b8cf34c04d1e17
Minopee <95720907+Minopee@users.noreply.github.com>
2025-08-17 03:02:37 +0300

    Add files via upload

---
+++ ar/vendor/mindar/mindar-face.prod.js
@@ -0,0 +1,12 @@
+import { C as o } from "./controller-c792144c.js";
+import { U as r } from "./ui-85e81035.js";
+const i = {
+  Controller: o,
+  UI: r
+};
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.FACE = i;
+export {
+  o as Controller,
+  r as UI
+};
